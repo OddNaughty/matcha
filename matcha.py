70d0bd41064236f6e200ab0fafa3d6cbec335d10
Camille WAGNER <cwagner@e3r11p1.42.fr>
2016-09-21 20:11:52 +0200

    All is working :')

--- matcha.py
+++ matcha.py
@@ -1,12 +1,27 @@
+# all the imports
+import os
+
 from flask import Flask
 
+
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'matcha.db'),
+    SECRET_KEY='+qd=_k!t!t1qr*c7%v-x_m_)%py^3ulb=@9-9sjo7je%3nwevy',
+    USERNAME='admin',
+    PASSWORD='admin'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
+from db import init_db
 
-if __name__ == '__main__':
-    app.run()
+@app.cli.command('initdb')
+def k():
+    init_db()
+    print('Database initialized')
